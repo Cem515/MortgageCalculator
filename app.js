259ee53c6607ab2c2c7091b8a37268e5e97913e7
Collin McCoy <collinmccoy17@gmail.com>
2017-04-27 14:28:29 -0700

    first angular addition

---
+++ app.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app', []);
+})();
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('MortgageCalculator', function($scope) {
+
+
+
+ });
+    MortgageCalculator.$inject = ['$scope'];
+
+
+    function MortgageCalculator($scope) {
+        var vm = this;
+        vm.output = "Shit"
+
+
+    }
+})();
