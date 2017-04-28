3a61ccdd11991a543c0de9895a2c43e914dd8ca4
Collin McCoy <collinmccoy17@gmail.com>
2017-04-28 14:38:54 -0700

    added notes

--- app.js
+++ app.js
@@ -7,14 +7,14 @@
 
 (function() {
   'use strict';
-
+  //Controller
   angular
     .module('app')
     .controller('MortgageController', MortgageController);
 
   MortgageController.$inject = [];
 
-  /* @ngInject */
+  //Function
   function MortgageController() {
     var vm = this;
     vm.balance = 0;
@@ -22,14 +22,14 @@
     vm.years = 0;
     vm.month = 12;
 
+    //Click Activation
+    vm.calculateMortgage = function() {
 
-  vm.calculateMortgage = function() {
-
-      var mir = ((vm.apr/100)/vm.month);
-      var nop = (vm.years*vm.month);
-      var cir = Math.pow((1+mir),nop);
-      var iq = (mir*cir)/(cir-1);
-      var final = Math.round(vm.balance*iq).toFixed(2);
+      var mir = ((vm.apr / 100) / vm.month);
+      var nop = (vm.years * vm.month);
+      var cir = Math.pow((1 + mir), nop);
+      var iq = (mir * cir) / (cir - 1);
+      var final = Math.round(vm.balance * iq).toFixed(2);
       vm.output = final
     }
   }
